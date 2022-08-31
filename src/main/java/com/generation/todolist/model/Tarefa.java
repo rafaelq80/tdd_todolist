e7a5ad5cfc16c08888c18eaa166f2fa205e54121
Rafael <rafaelproinfo@gmail.com>
2022-08-31 02:01:20 -0300

    versao 01

--- src/main/java/com/generation/todolist/model/Tarefa.java
+++ src/main/java/com/generation/todolist/model/Tarefa.java
@@ -29,7 +29,18 @@ public class Tarefa {
 	private LocalDate data;
 	
 	private Boolean status;
+	
+	public Tarefa(Long id, String nome, String descricao, String responsavel, LocalDate data, Boolean status) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.responsavel = responsavel;
+		this.data = data;
+		this.status = status;
+	}
 
+	public Tarefa() {	}
+	
 	public Long getId() {
 		return id;
 	}
